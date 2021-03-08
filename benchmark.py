dbc519306bc4456dfa5e5514d8531b24b89ff59c
Kevin Alex Zhang <kevz@mit.edu>
2021-03-08 01:48:14 -0800

    Added exrex benchmark.

--- benchmark.py
+++ benchmark.py
@@ -1,13 +1,14 @@
 import pandas as pd
+import exrex
 import intxeger
 from time import time
 from xeger import Xeger
 
 rows = []
 for nb_samples, regex in [
-    (100, "[a-zA-Z]+"),
-    (100, "[0-9]{3}-[0-9]{3}-[0-9]{4}"),
-    (1000, "[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}"),
+    (100, r"[a-zA-Z]+"),
+    (100, r"[0-9]{3}-[0-9]{3}-[0-9]{4}"),
+    (1000, r"[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}"),
     (1000, r"/json/([0-9]{4})/([a-z]{4})"),
 ]:
     row = {"N": nb_samples, "regex": regex}
@@ -15,14 +16,18 @@ for nb_samples, regex in [
     x = intxeger.build(regex)
     start = time()
     x.sample(nb_samples)
-    row["intxeger (ms)"] = round((time() - start)*1000, 2)
+    row["intxeger"] = round((time() - start)*1000, 2)
 
     x = Xeger()
     start = time()
     [x.xeger(regex) for _ in range(nb_samples)]
-    row["xeger (ms)"] = round((time() - start) * 1000, 2)
+    row["xeger"] = round((time() - start) * 1000, 2)
+
+    start = time()
+    [exrex.generate(regex) for _ in range(nb_samples)]
+    row["exrex"] = round((time() - start) * 1000, 2)
 
     rows.append(row)
-df = pd.DataFrame(rows, columns=["regex", "N", "xeger (ms)", "intxeger (ms)"])
+df = pd.DataFrame(rows, columns=["regex", "N", "xeger", "exrex", "intxeger"])
 
 print(df.to_markdown(index=False))
