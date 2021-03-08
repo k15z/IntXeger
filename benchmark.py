90993ed513977a01e3575ad5287b5879f72501e2
Kevin Alex Zhang <me@kevz.dev>
2021-03-07 20:05:50 -0800

    Added benchmark.

---
+++ benchmark.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import intxeger
+from time import time
+from xeger import Xeger
+
+rows = []
+for nb_samples, regex in [
+    (100, "[a-zA-Z]+"),
+    (100, "[0-9]{3}-[0-9]{3}-[0-9]{4}"),
+    (1000, "[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}"),
+    (10000, "[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}"),
+]:
+    row = {"N": nb_samples, "regex": regex}
+
+    x = intxeger.build(regex)
+    start = time()
+    x.sample(nb_samples)
+    row["intxeger (ms)"] = round((time() - start)*1000, 2)
+
+    x = Xeger()
+    start = time()
+    [x.xeger(regex) for _ in range(nb_samples)]
+    row["xeger (ms)"] = round((time() - start) * 1000, 2)
+
+    rows.append(row)
+df = pd.DataFrame(rows, columns=["regex", "N", "xeger (ms)", "intxeger (ms)"])
+
+print(df.to_markdown(index=False))
